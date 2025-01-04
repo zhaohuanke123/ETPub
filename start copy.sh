074cc4dc2f79c88734dd82dd9f9afbffc480ce11
JoVanko <1728931104@qq.com>
2025-01-04 15:51:26 +0800

    1

--- start copy.sh
+++ start copy.sh
@@ -5,9 +5,3 @@ cd /etc/ETPub/Server/publish || exit 1
 dotnet Server.dll --Process=1 --StartConfig=../Config/StartConfig/Release --Console=1 &
 SERVER_PID=$!
 echo $SERVER_PID > server_pid.txt  # 保存第一个服务的 PID
-
-# 启动第二个服务
-cd /etc/ETPub/FileServer/publish || exit 1
-dotnet FileServer.dll ../publish &
-FILESERVER_PID=$!
-echo $FILESERVER_PID > fileserver_pid.txt  # 保存第二个服务的 PID
