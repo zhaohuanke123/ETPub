b7637c4b09dfda1fcc533238d78a4165210f62c4
JoVanko <1728931104@qq.com>
2025-01-04 15:52:51 +0800

    1

--- start copy.sh
+++ start copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 启动第一个服务
-cd /etc/ETPub/Server/publish || exit 1
+cd /ETPub/Server/publish || exit 1
 dotnet Server.dll --Process=1 --StartConfig=../Config/StartConfig/Release --Console=1 &
 SERVER_PID=$!
 echo $SERVER_PID > server_pid.txt  # 保存第一个服务的 PID
