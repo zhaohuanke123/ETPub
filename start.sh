ec72da452d84e98a18d148be16019e0ae4bafcd5
JoVanko <1728931104@qq.com>
2025-01-08 09:03:13 +0800

    更新启动脚本

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-nohup sh -c 'cd /usr/mongodb && bin/mongod -f /etc/mongodb/conf/mongodb.conf' > /etc/mongodb/log/mongodb.log  &
+nohup sh -c 'cd /usr/mongodb && bin/mongod -f /etc/mongodb/conf/mongodb.conf' >/dev/null 2>&1  &
 echo "MongoDB 服务已启动。"
 
 # 启动第一个程序
-nohup sh -c 'cd /ETPub/Server/publish && dotnet Server.dll --Process=1 --StartConfig=../Config/StartConfig/Release --Console=1' > /ETPub/Server/publish/output.log  &
+nohup sh -c 'cd /ETPub/Server/publish && dotnet Server.dll --Process=1 --StartConfig=../Config/StartConfig/Release --Console=1' >/dev/null 2>&1  &
 
 echo "Server.dll 进程已启动。"
 
 # 启动第二个程序
-nohup sh -c 'cd /ETPub/FileServer/publish && dotnet FileServer.dll ../publish' > /ETPub/FileServer/publish/outputFS.log &
+nohup sh -c 'cd /ETPub/FileServer/publish && dotnet FileServer.dll ../publish' >/dev/null 2>&1  &
 
 echo "FileServer.dll 进程已启动。"
