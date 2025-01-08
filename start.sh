efe1bd3e6486c12ae5a48e32bdffff9be75365e4
JoVanko <1728931104@qq.com>
2025-01-08 19:29:41 +0800

    1 更新启动脚本 apptype = server

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ nohup sh -c 'cd /usr/mongodb && bin/mongod -f /etc/mongodb/conf/mongodb.conf' >/
 echo "MongoDB 服务已启动。"
 
 # 启动第一个程序
-nohup sh -c 'cd /ETPub/Server/publish && dotnet Server.dll --Process=1 --StartConfig=../Config/StartConfig/Release --Console=1' >/dev/null 2>&1  &
+nohup sh -c 'cd /ETPub/Server/publish && dotnet Server.dll --Process=1 --AppType=Server --StartConfig=../Config/StartConfig/Release --Console=1'>/dev/null 2>&1  &
 
 echo "Server.dll 进程已启动。"
 
